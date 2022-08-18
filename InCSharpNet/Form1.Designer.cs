e4ff1909c687f5fa37d3c06cfd6d665775a00009
Joachim Brack <joachimbrackh@gmail.com>
2022-08-18 09:27:10 +0200

    Überarbeitung

--- InCSharpNet/Form1.Designer.cs
+++ InCSharpNet/Form1.Designer.cs
@@ -16,6 +16,7 @@
         #region Vom Windows Form-Designer generierter Code
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBoxA = new System.Windows.Forms.TextBox();
             this.labelA = new System.Windows.Forms.Label();
             this.labelB = new System.Windows.Forms.Label();
@@ -30,11 +31,15 @@
             this.labelE = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.labelWarning2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelWarning3 = new System.Windows.Forms.Label();
+            this.labelNotification1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxA
             // 
-            this.textBoxA.Location = new System.Drawing.Point(70, 41);
+            this.textBoxA.Location = new System.Drawing.Point(36, 60);
             this.textBoxA.Name = "textBoxA";
             this.textBoxA.Size = new System.Drawing.Size(100, 20);
             this.textBoxA.TabIndex = 2;
@@ -42,31 +47,31 @@
             // labelA
             // 
             this.labelA.AutoSize = true;
-            this.labelA.Location = new System.Drawing.Point(70, 22);
+            this.labelA.Location = new System.Drawing.Point(33, 44);
             this.labelA.Name = "labelA";
             this.labelA.Size = new System.Drawing.Size(56, 13);
             this.labelA.TabIndex = 3;
-            this.labelA.Text = "Winkel (A)";
+            this.labelA.Text = "Winkel (α)";
             // 
             // labelB
             // 
             this.labelB.AutoSize = true;
-            this.labelB.Location = new System.Drawing.Point(67, 94);
+            this.labelB.Location = new System.Drawing.Point(33, 94);
             this.labelB.Name = "labelB";
-            this.labelB.Size = new System.Drawing.Size(94, 13);
+            this.labelB.Size = new System.Drawing.Size(90, 13);
             this.labelB.TabIndex = 4;
-            this.labelB.Text = "Schenkellänge (B)";
+            this.labelB.Text = "Schenkellänge (r)";
             // 
             // textBoxB
             // 
-            this.textBoxB.Location = new System.Drawing.Point(70, 110);
+            this.textBoxB.Location = new System.Drawing.Point(36, 110);
             this.textBoxB.Name = "textBoxB";
             this.textBoxB.Size = new System.Drawing.Size(100, 20);
             this.textBoxB.TabIndex = 5;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(70, 231);
+            this.button1.Location = new System.Drawing.Point(172, 56);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 6;
@@ -78,39 +83,39 @@
             // 
             this.labelWarning1.AutoSize = true;
             this.labelWarning1.ForeColor = System.Drawing.Color.Red;
-            this.labelWarning1.Location = new System.Drawing.Point(67, 266);
+            this.labelWarning1.Location = new System.Drawing.Point(33, 303);
             this.labelWarning1.Name = "labelWarning1";
-            this.labelWarning1.Size = new System.Drawing.Size(152, 13);
+            this.labelWarning1.Size = new System.Drawing.Size(148, 13);
             this.labelWarning1.TabIndex = 7;
-            this.labelWarning1.Text = "Geben Sie genau 2 Zahlen ein";
+            this.labelWarning1.Text = "Geben Sie genau 2 Werte ein";
             this.labelWarning1.Visible = false;
             // 
             // labelC
             // 
             this.labelC.AutoSize = true;
-            this.labelC.Location = new System.Drawing.Point(70, 165);
+            this.labelC.Location = new System.Drawing.Point(33, 144);
             this.labelC.Name = "labelC";
-            this.labelC.Size = new System.Drawing.Size(80, 13);
+            this.labelC.Size = new System.Drawing.Size(79, 13);
             this.labelC.TabIndex = 8;
-            this.labelC.Text = "Bogenlänge (C)";
+            this.labelC.Text = "Bogenlänge (b)";
             // 
             // textBoxC
             // 
-            this.textBoxC.Location = new System.Drawing.Point(70, 181);
+            this.textBoxC.Location = new System.Drawing.Point(36, 160);
             this.textBoxC.Name = "textBoxC";
             this.textBoxC.Size = new System.Drawing.Size(100, 20);
             this.textBoxC.TabIndex = 9;
             // 
             // textBoxD
             // 
-            this.textBoxD.Location = new System.Drawing.Point(218, 41);
+            this.textBoxD.Location = new System.Drawing.Point(36, 210);
             this.textBoxD.Name = "textBoxD";
             this.textBoxD.Size = new System.Drawing.Size(100, 20);
             this.textBoxD.TabIndex = 10;
             // 
             // textBoxE
             // 
-            this.textBoxE.Location = new System.Drawing.Point(218, 110);
+            this.textBoxE.Location = new System.Drawing.Point(36, 260);
             this.textBoxE.Name = "textBoxE";
             this.textBoxE.Size = new System.Drawing.Size(100, 20);
             this.textBoxE.TabIndex = 11;
@@ -118,48 +123,95 @@
             // labelD
             // 
             this.labelD.AutoSize = true;
-            this.labelD.Location = new System.Drawing.Point(215, 22);
+            this.labelD.Location = new System.Drawing.Point(33, 194);
             this.labelD.Name = "labelD";
-            this.labelD.Size = new System.Drawing.Size(96, 13);
+            this.labelD.Size = new System.Drawing.Size(93, 13);
             this.labelD.TabIndex = 12;
-            this.labelD.Text = "Sekantenlänge (D)";
+            this.labelD.Text = "Sekantenlänge (s)";
             // 
             // labelE
             // 
             this.labelE.AutoSize = true;
-            this.labelE.Location = new System.Drawing.Point(215, 94);
+            this.labelE.Location = new System.Drawing.Point(33, 244);
             this.labelE.Name = "labelE";
             this.labelE.Size = new System.Drawing.Size(86, 13);
             this.labelE.TabIndex = 13;
-            this.labelE.Text = "Flächeninhalt (E)";
+            this.labelE.Text = "Flächeninhalt (A)";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 231);
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(172, 89);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 14;
             this.button2.Text = "Skizziere";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Visible = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // labelWarning2
             // 
             this.labelWarning2.AutoSize = true;
             this.labelWarning2.ForeColor = System.Drawing.Color.Red;
-            this.labelWarning2.Location = new System.Drawing.Point(67, 279);
+            this.labelWarning2.Location = new System.Drawing.Point(33, 316);
             this.labelWarning2.Name = "labelWarning2";
             this.labelWarning2.Size = new System.Drawing.Size(216, 13);
             this.labelWarning2.TabIndex = 15;
-            this.labelWarning2.Text = "Der Winkel muss zwischen 0 und 360 liegen";
+            this.labelWarning2.Text = "Der Winkel muss zwischen 0 und 180 liegen";
             this.labelWarning2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(430, 13);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Geben Sie zwei Größen eines Kreisauschnitts an, um die Restlichen berechnen zu la" +
+    "ssen!";
+            // 
+            // labelWarning3
+            // 
+            this.labelWarning3.AutoSize = true;
+            this.labelWarning3.ForeColor = System.Drawing.Color.Red;
+            this.labelWarning3.Location = new System.Drawing.Point(33, 329);
+            this.labelWarning3.Name = "labelWarning3";
+            this.labelWarning3.Size = new System.Drawing.Size(174, 13);
+            this.labelWarning3.TabIndex = 17;
+            this.labelWarning3.Text = "Die Werte dürfen nicht negativ sein";
+            this.labelWarning3.Visible = false;
+            // 
+            // labelNotification1
+            // 
+            this.labelNotification1.AutoSize = true;
+            this.labelNotification1.Location = new System.Drawing.Point(33, 290);
+            this.labelNotification1.Name = "labelNotification1";
+            this.labelNotification1.Size = new System.Drawing.Size(301, 13);
+            this.labelNotification1.TabIndex = 18;
+            this.labelNotification1.Text = "Hinweiß: Bei dem Ergebnis handelt es sich um eine Schätzung";
+            this.labelNotification1.Visible = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 122);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Leeren";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Linen;
+            this.ClientSize = new System.Drawing.Size(685, 358);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.labelNotification1);
+            this.Controls.Add(this.labelWarning3);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.labelWarning2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.labelE);
@@ -174,8 +226,9 @@
             this.Controls.Add(this.labelB);
             this.Controls.Add(this.labelA);
             this.Controls.Add(this.textBoxA);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Rechner für Kreisausschnitt";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -196,6 +249,10 @@
         private System.Windows.Forms.Label labelE;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label labelWarning2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelWarning3;
+        private System.Windows.Forms.Label labelNotification1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- InCSharpNet/Form1.cs
+++ InCSharpNet/Form1.cs
@@ -16,7 +16,6 @@ namespace InCSharpNet
         public float C = 0;
         public float D = 0;
         public float E = 0;
-        public float height = 0;
         public int valueCounter = 0;
 
         private void button1_Click(object sender, EventArgs e)
@@ -65,14 +64,23 @@ namespace InCSharpNet
                 valueCounter++;
             }
 
-            if (A < 0 || A > 360)
+            if (A < 0 || A > 180)
             {
                 labelWarning2.Visible = true;
             }
 
-            if (A >= 0 && A <= 360)
+            if (A >= 0 && A <= 180)
             {
                 labelWarning2.Visible = false;
+            }
+
+            if (B < 0 || C < 0 || D < 0 || E < 0 )
+            {
+                labelWarning3.Visible = true;
+            }
+            else
+            {
+                labelWarning3.Visible = false;
             }
 
             if (valueCounter != 2)
@@ -80,12 +88,12 @@ namespace InCSharpNet
                 labelWarning1.Visible = true;
             }
 
-            if (valueCounter == 2 && (A >= 0 && A <= 360))
+            if (valueCounter == 2 && (A >= 0 && A <= 180))
             {
-                button2.Visible = true;
+                button2.Enabled = true;
                 labelWarning1.Visible = false;
 
-                if (aIsTrue && bIsTrue)
+                if (aIsTrue && bIsTrue && B >= 0)
                 {
                     C = B * (float)Math.PI * (A / 180);
                     D = 2 * B * (float)Math.Sin((A / 2) * (Math.PI / 180));
@@ -95,7 +103,7 @@ namespace InCSharpNet
                     textBoxD.Text = D.ToString();
                     textBoxE.Text = E.ToString();
                 }
-                else if (aIsTrue && cIsTrue)
+                else if (aIsTrue && cIsTrue && C >= 0)
                 {
                     B = (C * 180) / (A * (float)Math.PI);
                     D = 2 * B * (float)Math.Sin((A / 2) * (Math.PI / 180));
@@ -106,7 +114,7 @@ namespace InCSharpNet
                     textBoxE.Text = E.ToString();
 
                 }
-                else if (aIsTrue && dIsTrue)
+                else if (aIsTrue && dIsTrue && D >= 0)
                 {
                     B = D / (2 * (float)Math.Sin((A / 2) * (Math.PI / 180)));
                     C = B * (float)Math.PI * (A / 180);
@@ -116,9 +124,9 @@ namespace InCSharpNet
                     textBoxC.Text = C.ToString();
                     textBoxE.Text = E.ToString();
                 }
-                else if (aIsTrue && eIsTrue)
+                else if (aIsTrue && eIsTrue && E >= 0)
                 {
-                    B = (float)Math.Sqrt(E / Math.PI / 360 * A);
+                    B = (float)Math.Sqrt((E * 360) / ((float)Math.PI * A));
                     C = B * (float)Math.PI * (A / 180);
                     D = 2 * B * (float)Math.Sin((A / 2) * (Math.PI / 180));
 
@@ -126,7 +134,7 @@ namespace InCSharpNet
                     textBoxC.Text = C.ToString();
                     textBoxD.Text = D.ToString();
                 }
-                else if (bIsTrue && cIsTrue)
+                else if (bIsTrue && cIsTrue && B >= 0 && C >= 0)
                 {
                     A = C / B * (180 / (float)Math.PI);
                     D = 2 * B * (float)Math.Sin((A / 2) * (Math.PI / 180));
@@ -136,9 +144,9 @@ namespace InCSharpNet
                     textBoxD.Text = D.ToString();
                     textBoxE.Text = E.ToString();
                 }
-                else if (bIsTrue && dIsTrue)
+                else if (bIsTrue && dIsTrue && B >= 0 && D >= 0)
                 {
-                    A = (2 * B - (float)Math.Sqrt((4 * B * B) - D * D)) / 2;
+                    A = 2 * (float)Math.Asin(D / (2 * B)) * (180 / (float)Math.PI);
                     C = B * (float)Math.PI * (A / 180);
                     E = B * B * (float)Math.PI * (A / 360);
 
@@ -146,7 +154,7 @@ namespace InCSharpNet
                     textBoxC.Text = C.ToString();
                     textBoxE.Text = E.ToString();
                 }
-                else if (bIsTrue && eIsTrue)
+                else if (bIsTrue && eIsTrue && B >= 0 && E >= 0)
                 {
                     A = E / (B * B) / (float)Math.PI * 360;
                     C = B * (float)Math.PI * (A / 180);
@@ -156,26 +164,65 @@ namespace InCSharpNet
                     textBoxC.Text = C.ToString();
                     textBoxD.Text = D.ToString();
                 }
-                else if (cIsTrue && dIsTrue)
+                else if (cIsTrue && dIsTrue && C >= 0 && D >= 0)
                 {
-                    height = C - D;
-                    B = ((4 * height * height) + (D * D)) / (8 * height);
-                    A = C / B * (180 / (float)Math.PI);
+                    float temp1 = 0;
+                    float temp2 = 100;
+                    for(double i = 1; i < 181; i = i + 0.1)
+                    {
+                        temp1 = (C * 2 * (float)Math.Sin(((float)i/2) * (Math.PI / 180))) - ((float)i * ((float)Math.PI / 180) * D);
+                        if (temp1 < 0)
+                        {
+                            temp1 = temp1 * -1;
+                        }
+                        if(temp1 < temp2)
+                        {
+                            temp2 = temp1;
+                            A = (float)i;
+                        }
+                    }
+
+                    B = (C * 180) / (A * (float)Math.PI);
                     E = B * B * (float)Math.PI * (A / 360);
+
+                    labelNotification1.Visible = true;
 
                     textBoxA.Text = A.ToString();
                     textBoxB.Text = B.ToString();
                     textBoxE.Text = E.ToString();
                 }
-                else if (cIsTrue && eIsTrue)
+                else if (cIsTrue && eIsTrue && C >= 0 && E >= 0)
                 {
+                    B = (E * 2) / C;
+                    A = E / (B * B) / (float)Math.PI * 360;
+                    D = 2 * B * (float)Math.Sin((A / 2) * (Math.PI / 180));
 
                     textBoxA.Text = A.ToString();
                     textBoxB.Text = B.ToString();
                     textBoxD.Text = D.ToString();
                 }
-                else if (dIsTrue && eIsTrue)
+                else if (dIsTrue && eIsTrue && D >= 0 && E >= 0)
                 {
+                    float temp1 = 0;
+                    float temp2 = 100;
+                    for(double i = 1; i < 181 ; i = i + 0.1)
+                    {
+                        temp1 = (D / (2 * (float)Math.Sin((i / 2) * (Math.PI / 180)))) - ((float)Math.Sqrt((E * 360) / ((float)Math.PI * i)));
+                        if (temp1 < 0)
+                        {
+                            temp1 = temp1 * -1;
+                        }
+                        if(temp1 < temp2)
+                        {
+                            temp2 = temp1;
+                            A = (float)i;
+                        }
+                    }
+
+                    B = (float)Math.Sqrt((E * 360) / ((float)Math.PI * A));
+                    C = B * (float)Math.PI * (A / 180);
+
+                    labelNotification1.Visible = true;
 
                     textBoxA.Text = A.ToString();
                     textBoxB.Text = B.ToString();
@@ -185,24 +232,72 @@ namespace InCSharpNet
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            Point p1, p2;
-            Pen myPen = new Pen(Color.Black, 1);
+            string aText = textBoxA.Text;
+            Single.TryParse(aText, out A);
 
-            p1 = new Point();
-            p2 = new Point();
+            if (A >= 0 && A <= 180)
+            {
+                Point p1, p2;
+                Pen myPen1 = new Pen(Color.Black, 1);
+                Pen myPen2 = new Pen(Color.OrangeRed, 2);
+                Pen myPen3 = new Pen(Color.Blue, 2);
+                Pen myPen4 = new Pen(Color.DarkOrchid, 2);
+                Pen myPen5 = new Pen(Color.ForestGreen, 2);
 
-            p1.X = 500;
-            p1.Y = 180;
-            p2.X = 650;
-            p2.Y = 180;
+                Font drawFont = new Font("Arial", 15);
 
-            float p3X = ((float)Math.Cos(A * (Math.PI / 180))) * 150;
-            float p3Y = ((float)Math.Sin(A * (Math.PI / 180))) * 150;
+                SolidBrush drawBrush2 = new SolidBrush(Color.OrangeRed);
+                SolidBrush drawBrush3 = new SolidBrush(Color.Blue);
+                SolidBrush drawBrush4 = new SolidBrush(Color.DarkOrchid);
+                SolidBrush drawBrush5 = new SolidBrush(Color.ForestGreen);
 
-            Graphics g = this.CreateGraphics();
-            g.DrawLine(myPen, p1, p2);
-            g.DrawLine(myPen, p1.X, p1.Y, p3X+p1.X, -p3Y+p1.Y);
-            g.DrawArc(myPen, 350, 30, 300, 300, 0, -A);
+                StringFormat drawFormat = new StringFormat();
+                drawFormat.Alignment = StringAlignment.Center;
+
+                p1 = new Point();
+                p2 = new Point();
+
+                p1.X = 500;
+                p1.Y = 180;
+                p2.X = 650;
+                p2.Y = 180;
+
+                float p3X = ((float)Math.Cos(A * (Math.PI / 180))) * 150;
+                float p3Y = ((float)Math.Sin(A * (Math.PI / 180))) * 150;
+
+                Graphics g = this.CreateGraphics();
+                g.Clear(Color.Linen);
+                g.DrawLine(myPen2, p1, p2);
+                g.DrawLine(myPen2, p1.X, p1.Y, p3X + p1.X, -p3Y + p1.Y);
+                g.DrawLine(myPen5, p2.X, p2.Y, p3X + p1.X, -p3Y + p1.Y);
+                g.DrawArc(myPen3, 350, 30, 300, 300, 0, -A);
+                g.DrawArc(myPen4, 470, 150, 60, 60, 0, -A);
+                g.DrawArc(myPen1, 350, 30, 300, 300, 0, 360);
+                g.DrawString("r", drawFont, drawBrush2, p2.X - 90, p2.Y - 3, drawFormat);
+                g.DrawString("α", drawFont, drawBrush4, p1.X + 20, p1.Y - 21, drawFormat);
+                g.DrawString("s", drawFont, drawBrush5, p2.X + 10, p2.Y - 16, drawFormat);
+                g.DrawString("b", drawFont, drawBrush3, p3X + p1.X - 17, -p3Y + p1.Y - 23, drawFormat);
+            }
+
+            if (A < 0 || A > 180)
+            {
+                labelWarning2.Visible = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            A = 0;
+            B = 0;
+            C = 0;
+            D = 0;
+            E = 0;
+
+            textBoxA.Text = null;
+            textBoxB.Text = null;
+            textBoxC.Text = null;
+            textBoxD.Text = null;
+            textBoxE.Text = null;
         }
     }
 }
